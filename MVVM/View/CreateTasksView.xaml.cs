9a0c5f15c1ebc3ad63b38641dd097285ca6d31d7
tarnaldo88 <tarnaldo88@gmail.com>
2024-12-13 14:21:03 -0800

    Starting creating tasks View

---
+++ MVVM/View/CreateTasksView.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace MenuDesign.MVVM.View
+{
+    /// <summary>
+    /// Interaction logic for CreateTasksView.xaml
+    /// </summary>
+    public partial class CreateTasksView : UserControl
+    {
+        public CreateTasksView()
+        {
+            InitializeComponent();
+        }
+    }
+}
