38ffdf4b8fc166b8480d3089c179413f45acc75a
tarnaldo88 <tarnaldo88@gmail.com>
2024-12-13 17:13:05 -0800

    Setting up task object

---
+++ Services/ViewModelLocator.cs
@@ -0,0 +1,23 @@
+﻿using MenuDesign.MVVM.ViewModel;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace MenuDesign.Services
+{
+    internal class ViewModelLocator
+    {
+        private readonly IServiceProvider _provider;
+        public ViewModelLocator(IServiceProvider provider)
+        {
+            _provider = provider;
+        }
+
+        public MainViewModel MainViewModel => _provider.GetRequiredService<MainViewModel>();
+        public SettingsViewModel SettingsViewModel => _provider.GetRequiredService<SettingsViewModel>();
+    }
+}
