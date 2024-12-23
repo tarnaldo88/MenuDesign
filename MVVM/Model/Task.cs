5421bbca293b68a2e8c4156ef5aab9ef5c66550a
tarnaldo88 <tarnaldo88@gmail.com>
2024-12-22 20:33:36 -0800

    added task serializable class

---
+++ MVVM/Model/Task.cs
@@ -0,0 +1,47 @@
+﻿
+using System;
+using System.IO;
+using System.Xml.Serialization;
+
+namespace MenuDesign.MVVM.Model
+{
+    [Serializable]
+    internal class Task
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public DateTime DueDate { get; set; }
+        public bool IsCompleted { get; set; }
+
+
+        public Task( string name, string desc, int id, DateTime dueDate, bool completed)
+        {
+            Title = name;
+            Description = desc;
+            Id = id;
+            DueDate = dueDate;
+            IsCompleted = completed;
+        }
+
+        // Save to XML file
+        public void SaveToXml(string filePath)
+        {
+            XmlSerializer serializer = new XmlSerializer(typeof(Task));
+            using (StreamWriter writer = new StreamWriter(filePath))
+            {
+                serializer.Serialize(writer, this);
+            }
+        }
+
+        // Load from XML file
+        public static Task LoadFromXml(string filePath)
+        {
+            XmlSerializer serializer = new XmlSerializer(typeof(Task));
+            using (StreamReader reader = new StreamReader(filePath))
+            {
+                return (Task)serializer.Deserialize(reader);
+            }
+        }
+    }
+}
