f251ffff7ea1f04f5ea36a08cdbe3640d62254cd
tarnaldo88 <tarnaldo88@gmail.com>
2026-03-26 23:12:58 -0700

    fix

--- MVVM/ViewModel/CompletedTasksViewModel.cs
+++ MVVM/ViewModel/CompletedTasksViewModel.cs
@@ -8,5 +8,42 @@ namespace MenuDesign.MVVM.ViewModel
 {
     internal class CompletedTasksViewModel
     {
+
+        public string LongestCommonPrefix(string[] strs)
+        {
+            if (strs.Length == 0)
+            {
+                return "";
+            } 
+            else if(strs.Length == 1)
+            {
+                return strs[0];
+            }
+
+            StringBuilder sb = new StringBuilder();
+
+            for (int i = 0; i< strs.Length; i++)
+            {
+                for (int j = i + 1; j < strs[i].Length; j++)
+                {
+                    if(i + 1 < strs.Length && j < strs[i+1].Length)
+                    {
+                        if (strs[i][j].Equals(strs[i + 1][j]))
+                        {
+                            sb.Append(strs[i][j]);                           
+                        } 
+                        else
+                        {
+                            break;
+                        }
+                    }
+                }
+            }
+            
+
+            return sb.ToString();
+        }
     }
 }
+
+// 780
